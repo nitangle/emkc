f6f9e4b4305648784390e68da54e4ce806e9aed8
Brian Seymour <imbrianseymour@gmail.com>
2020-06-03 15:53:55 -0500

    experimental new challenge structure in progress

---
+++ platform/migrations/migrations/20200603075420-update-challenge-names.sql
@@ -0,0 +1,29 @@
+up:
+update challenges set folder = '1_reverse_string.json' where challenge_id = 1;
+update challenges set folder = '2_number_frequency.json' where challenge_id = 2;
+update challenges set folder = '3_list_sum.json' where challenge_id = 3;
+update challenges set folder = '4_invalid_json.json' where challenge_id = 4;
+update challenges set folder = '5_binary_convert.json' where challenge_id = 5;
+update challenges set folder = '6_fibonacci_sequence.json' where challenge_id = 6;
+update challenges set folder = '7_check_permutation.json' where challenge_id = 7;
+update challenges set folder = '8_sort_integers.json' where challenge_id = 8;
+update challenges set folder = '9_roman_numerals.json' where challenge_id = 9;
+update challenges set folder = '10_pig_latin.json' where challenge_id = 10;
+update challenges set folder = '11_array_neighbor.json' where challenge_id = 11;
+update challenges set folder = '12_longest_word.json' where challenge_id = 12;
+update challenges set folder = '13_most_common_character.json' where challenge_id = 13;
+
+down:
+update challenges set folder = '1_reverse_string' where challenge_id = 1;
+update challenges set folder = '2_number_frequency' where challenge_id = 2;
+update challenges set folder = '3_list_sum' where challenge_id = 3;
+update challenges set folder = '4_invalid_json' where challenge_id = 4;
+update challenges set folder = '5_binary_convert' where challenge_id = 5;
+update challenges set folder = '6_fibonacci_sequence' where challenge_id = 6;
+update challenges set folder = '7_check_permutation' where challenge_id = 7;
+update challenges set folder = '8_sort_integers' where challenge_id = 8;
+update challenges set folder = '9_roman_numerals' where challenge_id = 9;
+update challenges set folder = '10_pig_latin' where challenge_id = 10;
+update challenges set folder = '11_array_neighbor' where challenge_id = 11;
+update challenges set folder = '12_longest_word' where challenge_id = 12;
+update challenges set folder = '13_most_common_character' where challenge_id = 13;
